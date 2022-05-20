17e9e615dfe832f76f2c8772ad85c88043c48916
Darren Yip <drrnyip@gmail.com>
2022-05-20 15:57:04 +0800

    landing page with dark mode mechanism

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Navbar :darkMode="darkMode" @toggleDarkMode="toggleDarkMode" />
+  <Landing :darkMode="darkMode" />
+</template>
+
+
+<script setup lang="ts">
+import Navbar from './components/Navbar.vue'
+import Landing from './components/Landing.vue'
+import { ref } from "vue"
+let darkMode = ref(true);
+
+function toggleDarkMode() {
+  darkMode.value = !darkMode.value;
+}
+</script>
+
+
+
+<style>
+#app {}
+</style>
