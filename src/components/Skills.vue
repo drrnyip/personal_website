2a8dbd141ddd58e05f9f0d9cd4f8dfe85429d25d
Darren Yip <drrnyip@gmail.com>
2022-05-22 14:13:16 +0800

    feat(skills)

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,19 +1,19 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="py-12 bg-white">
+  <div class="py-12 bg-white md:mx-24 lg:mx-36">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="lg:text-center">
-        <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase text-center md:text-left">About</h2>
+        <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase text-center md:text-left">Skills</h2>
         <!-- <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">A better way to send money</p> -->
         <!-- <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p> -->
       </div>
 
       <div class="mt-10">
-        <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-          <div v-for="skill in skills" :key="skill.name" class="relative">
+        <!-- <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10"> -->
+        <dl class="flex flex-col md:flex-row flex-wrap justify-around">
+          <div v-for="skill in skills" :key="skill.name" class="relative basis-1/2 mt-4 mx-2 md:mx-0">
             <dt>
               <div class="absolute flex items-center justify-center h-12 w-12 rounded-md text-white">
-                <!-- <component :is="skill.icon" class="h-6 w-6" aria-hidden="true" /> -->
                 <img class="h-8 lg:h-12" :src="skill.icon" />
               </div>
               <p class="ml-16 text-lg leading-6 font-medium text-gray-900">{{ skill.name }}</p>
