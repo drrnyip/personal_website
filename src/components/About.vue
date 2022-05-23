5197b461a09b241566444ad261e0640c0caa9116
Darren Yip <drrnyip@gmail.com>
2022-05-23 22:21:32 +0800

    feat(copy)

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,6 @@
 <template>
     <div id="about" class="py-12 bg-gray-100 dark:bg-gray-800 md:mx-24 lg:mx-36">
-                <h2 class="text-center md:text-left text-3xl md:text-3xl lg:text-4xl font-sans text-red-700 dark:text-yellow-500 tracking-widest font-semibold my-4 px-4">About me</h2>
-
-        <!-- <h2
-            class="text-base text-red-700 dark:text-yellow-500 font-semibold tracking-wide uppercase text-center md:text-left">
-            About me</h2> -->
+                <h2 class="text-center md:text-left text-3xl md:text-3xl lg:text-4xl font-sans text-red-700 dark:text-yellow-500 tracking-widest font-semibold my-4 px-4">About Me</h2>
         <div class="flex flex-col md:flex-row my-10">
             <img src="../assets/logo.png"
                 class="cover m-auto md:mr-8 h-48 w-48 ring-2 ring-red-700 dark:ring-yellow-500 rounded-full" />
@@ -17,11 +13,11 @@
                 <p>
                     I've been building web applications since 2015, working for <a href="https://www.checkmate.digital"
                         class="underline" target="_blank">smaller startups</a> to <a class="underline"
-                        href="https://www.nyse.com/index" target="_blank">large corporations</a>. My interest lies in
-                    creating engaging and memorable digital experiences for users.
+                        href="https://www.nyse.com/index" target="_blank">large corporations</a>. My primary interest lies in
+                    creating engaging and memorable digital experiences for people.
                 </p>
                 <br />
-                <p>When not in front of a computer, you can usually find me in front of a piano, or with my nose in a book.</p>
+                <p>When not in front of a computer, I can usually be found in front of a piano, or with my nose in a book.</p>
             </div>
         </div>
     </div>
