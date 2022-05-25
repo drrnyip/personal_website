f830a14f83158bfa7cf6ec4acbb08e0ea529982a
Darren Yip <drrnyip@gmail.com>
2022-05-25 18:19:33 +0800

    feat(social): dynamic entry

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="flex flex-row justify-center w-full md:w-fit md:flex-col md:fixed absolute md:bottom-20 md:left-14 cursor-pointer">
+    <div id="social" class="flex flex-row justify-center w-full md:w-fit md:flex-col md:fixed absolute md:bottom-20 md:left-14 cursor-pointer"
+        :class="{ entry: entered }">
         <div v-for="icon in icons" :key="icon.id"
             class="icon relative font-sans tracking-wider mb-4 mx-4 md:mx-0 hover:opacity-70"
             @mouseenter="setHover(icon.id)" @mouseleave="stopHover(icon.id)">
             <a :alt="icon.id" :href="icon.href" class="" target="_blank">
                 <img :src="darkMode ? icon.darkImg : icon.lightImg" class="h-8 w-8" />
             </a>
-            <p class="tooltip absolute cursor-pointer hidden md:block" :class="{ show: isHovering[icon.id]}">{{ icon.description }}</p>
+            <p class="tooltip absolute cursor-pointer hidden md:block" :class="{ show: isHovering[icon.id] }">{{
+                    icon.description
+            }}</p>
         </div>
         <div id="vertical" class="hidden md:block"></div>
         <!-- <div id="horizontal" class="block md:hidden"></div> -->
@@ -14,7 +17,7 @@
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import { ref, onMounted } from 'vue';
 import mailYellow from "../assets/mail_yellow.png";
 import mailRed from "../assets/mail_red.png";
 import githubYellow from "../assets/github_yellow.png";
@@ -29,6 +32,13 @@ const props = defineProps({
 })
 
 const isHovering = ref({});
+const entered = ref(false);
+
+onMounted(() => {
+    setTimeout(() => {
+        entered.value = true;
+    }, 1000)
+})
 
 const icons = [
     {
@@ -100,12 +110,6 @@ function stopHover(id) {
     border-bottom: 2px solid #4b5563;
 }
 
-.icon:hover {
-    /* transform: translateY(-3px); */
-    /* transition: all 0.1s ease-in; */
-    /* animation: bounce 1s; */
-}
-
 .tooltip {
     color: white;
     top: 20%;
@@ -118,5 +122,14 @@ function stopHover(id) {
 .show {
     opacity: 1;
     transform: translateX(0%);
+}
+
+#social {
+    transform: translateY(200%);
+    transition: transform 0.5s ease;
+}
+
+#social.entry {
+    transform: translateY(0%);
 }
 </style>
