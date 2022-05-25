837df823274906fd1dad616b87ef224921a9b3ef
Darren Yip <drrnyip@gmail.com>
2022-05-26 00:13:54 +0800

    remove vue-gtag

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import AosVue from "aos-vue";
-import VueGtag from "vue-gtag";
 
 createApp(App)
-  .use(VueGtag, {
-    config: { id: "G-8GHVNSQPNS" },
-  })
   .use(AosVue)
   .mount("#app");
