542383f4d932c3a05b2e8f2cd589a4a7b3694e76
Darren Yip <drrnyip@gmail.com>
2022-05-25 20:18:48 +0800

    feat(vue-gtag): vue google analytics

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css';
 import AosVue from "aos-vue";
+import VueGtag from "vue-gtag";
 
-createApp(App).use(AosVue).mount('#app')
+createApp(App).use(VueGtag, {
+    config: { id: "G-8GHVNSQPNS" }
+  }).use(AosVue).mount('#app')
