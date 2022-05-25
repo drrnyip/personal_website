d08454c033ede9fe20bd2f9659dd0b70b08f014e
Darren Yip <drrnyip@gmail.com>
2022-05-25 20:23:38 +0800

    feat(vue-gtag): vue google analytics fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
 import AosVue from "aos-vue";
 import VueGtag from "vue-gtag";
 
-createApp(App).use(VueGtag, {
-    config: { id: "G-8GHVNSQPNS" }
-  }).use(AosVue).mount('#app')
+createApp(App)
+  .use(VueGtag, {
+    config: { id: "G-8GHVNSQPNS" },
+  })
+  .use(AosVue)
+  .mount("#app");
