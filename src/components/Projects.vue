b2961f39cbc5f6e4a4a9443855c113714758ac99
Darren Yip <drrnyip@gmail.com>
2022-06-08 16:51:32 +0800

    feat(styling)

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,19 +4,19 @@
             class="text-center md:text-left text-3xl md:text-3xl lg:text-4xl font-sans text-red-700 dark:text-yellow-500 tracking-widest font-semibold my-4 px-4">
             Things I've Built
         </h2>
-        <div class="flex flex-col">
-            <div v-for="project of projects" :key="project.name" class="flex flex-col md:flex-row">
+        <div class="flex flex-col mt-8">
+            <div v-for="project of projects" :key="project.name" class="project flex flex-col md:flex-row">
                 <div class="screenshot rounded border-red-700 dark:border-yellow-500 border-2 mx-6 md:mx-2 basis-2/5 md:basis-3/5 flex-1 cursor-pointer">
                     <img :src="project.img" class="object-contain h-full w-full mix-blend-difference hover:mix-blend-saturate backdrop-grayscale hover:backdrop-grayscale-0 backdrop-contrast-200 hover:backdrop-contrast-100 backdrop-brightness-sm hover:backdrop-brightness " :alt="project.alt" @click="newtab(project.href)"/>
                 </div>
-                <div class="details basis-2/5 flex-1 text-center md:text-right z-10">
+                <div class="details basis-2/5 flex-1 px-4 md:px-0 text-left md:text-right z-10">
                     <h1 class="text-2xl text-red-700 dark:text-yellow-500 font-sans tracking-wider mt-4 md:mt-10 mb-2 md:mb-6">{{project.name}}</h1>
                     <p class="w-full md:w-[120%] md:ml-[-20%] text-md py-4 px-6 rounded text-gray-800 dark:text-gray-50 bg-gray-200 dark:bg-gray-900">{{project.description}}</p>
-                    <div class="flex flex-row md:flex-col justify-between mx-2 md:mx-0 mt-2 md:mt-4">
-                        <ul class="flex justify-end">
+                    <div class="deets relative flex flex-row justify-between md:justify-end px-0 md:px-2 mx-2 md:mx-0 mt-2 md:mt-4">
+                        <ul class="flex justify-around">
                             <li v-for="used of project.builtwith" :key="used" class="mx-2 font-mono text-xs">{{used}}</li>
                         </ul>
-                        <div class="flex md:my-4 md:mx-2 justify-end">
+                        <div class="flex md:mx-2 justify-around">
                             <img :src="darkMode ? link_yellow: link_red" class="h-4 cursor-pointer" @click="newtab(project.href)"/>
                         </div>
                     </div>
@@ -53,7 +53,7 @@ const projects = ref([
         href: 'https://justgimmeapassword.com',
         // img: 'justgimme',
         img: justgimme,
-        description: 'A minimalist password generator for quick password generation. Randomly generates easy-to-remember passwords that satisfies current password security standards.',
+        description: 'A web application for quickly generating passwords. Randomly generates easy-to-remember passwords that satisfies modern security standards.',
         builtwith: ['Vue 3', 'JS', 'Vercel']
     }
 ])
@@ -73,8 +73,16 @@ function newtab(href: string) {
 </script>
 
 <style scoped>
-.screenshot {
-    transition: all1s ease;
-    /* background: rgba(255,255,255, 0.5); */
-}
+/* .deets:after {
+    z-index: -1;
+    content: "";
+    height: 25px;
+    width: 25px;
+    position: absolute;
+    right: 0;
+    bottom: -100%;
+    border-radius: 0 0 5px 0;
+    border-right: 2px #111827 solid;
+    border-bottom: 2px #111827 solid;
+} */
 </style>
