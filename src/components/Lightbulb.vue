176417e110212e25c0a2ca23a3b45bfdd10d3fdc
Darren Yip <drrnyip@gmail.com>
2022-06-15 16:47:25 +0800

    feat(lightbulb): add hint to indicate lightbulb can be clicked

--- src/components/Lightbulb.vue
+++ src/components/Lightbulb.vue
@@ -1,11 +1,17 @@
 <template>
     <div id="bulbcontainer"
         class="fixed right-8 sm:right-12 md:right-14 lg:right-24 xl:right-36 2xl:right-52 -top-28 dark:-top-32 dark:hover:-top-28 hover:-top-28 md:top-0 md:dark:-top-4 md:dark:hover:top-0 md:hover:top-0 items-center pr-2 h-fit"
-        :class="{ entry: entered }" @click="emitDarkMode">
+        :class="{ entry: entered }" @click="emitDarkMode" @mouseover="activateHint" @mouseleave="deactivateHint">
         <span class="sr-only">Toggle dark mode</span>
         <div id="switch" class="bg-gray-600"></div>
         <img id="bulb" class="h-8 w-8 rotate-180" :src="darkMode ? (flickering) ? lightOn : lightOff : lightOn"
             aria-hidden="true" />
+        <div id="lightbulb-hint" class="font-sans text-center tracking-tight font-medium text-sm w-[80px] text-gray-800 dark:text-gray-50"
+            :class="{ show: !clickedAtLeastOnce && showHint }">
+            <span>Too dark?</span>
+            <br />
+            <span>Hit the lights</span>
+        </div>
     </div>
 </template>
 
@@ -20,6 +26,9 @@ const props = defineProps({
 
 const flickering = ref(false);
 const entered = ref(false);
+const clickedAtLeastOnce = ref(false);
+const showHint = ref(false);
+const showHintInitialDelay = 15000; // in ms
 
 onMounted(() => {
     setTimeout(() => {
@@ -31,12 +40,29 @@ onMounted(() => {
             flickering.value = false;
         }, Math.floor((Math.random() * 500) + 100))
     }, Math.floor((Math.random() * 20000) + 1000))
+
+    // Show hint after set delay
+    setTimeout(() => {
+        showHint.value = true;
+        setTimeout(() => {
+            showHint.value = false;
+        }, 5000)
+    }, showHintInitialDelay);
 })
 
 const emits = defineEmits(['toggleDarkMode'])
 
 function emitDarkMode() {
+    clickedAtLeastOnce.value = true;
     emits('toggleDarkMode', true);
+}
+
+function activateHint() {
+    showHint.value = true;
+}
+
+function deactivateHint() {
+    showHint.value = false;
 }
 </script>
 
@@ -45,7 +71,7 @@ function emitDarkMode() {
     width: 50px;
     cursor: pointer;
     z-index: 50;
-    transition: all 0.25s ease-in-out;
+    transition: all 0.75s ease-in-out;
     transform: translateY(-200%);
 }
 
@@ -66,5 +92,18 @@ function emitDarkMode() {
 #bulb {
     position: relative;
     cursor: pointer;
+}
+
+#lightbulb-hint {
+    transition: all 0.5s ease;
+    margin-left: -50%;
+    margin-top: 10px;
+    transform: translateY(-25%);
+    opacity: 0;
+}
+
+.show {
+    opacity: 1 !important;
+    transform: translateY(0%) !important;
 }
 </style>
