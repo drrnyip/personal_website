e1cb44534d57e07c940fb042401f69bf1e2f91aa
Darren Yip <drrnyip@gmail.com>
2023-08-12 21:52:35 -0400

    Change mobile escalator to accommodate more employers

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -3,50 +3,66 @@
         <aos-vue animation="fade-in" :once="true" placement="center-bottom">
             <h2
                 class="text-center md:text-left text-3xl md:text-3xl lg:text-4xl font-sans text-red-700 dark:text-yellow-500 tracking-widest font-semibold my-4 px-4">
-                Previous Workplaces</h2>
+                Where I've Worked</h2>
         </aos-vue>
         <div class="flex flex-col md:flex-row h-fit w-fit mx-6 md:mx-0 justify-around tablist py-5">
             <aos-vue animation="fade-right" :once="true" placement="center-bottom" :duration="750"
                 class="flex flex-row md:flex-col basis-2/6 tracking-wide cursor-pointer">
                 <ul
-                    class="flex flex-row justify-around w-[90vw] md:w-full md:flex-col border-b-4 md:border-b-0 md:border-l-4 md:mr-5 md:pr-5">
+                    class="hidden md:flex md:flex-row justify-around w-[80vw] sm:w-[85vw] md:w-full md:flex-col md:border-b-0 md:border-l-4 md:mr-5 md:pr-5">
                     <li v-for="(experience, index) in experiences" :key="`${experience.title}_${experience.employer}`"
-                        class="box-border cursor-pointerfont-sans h-[60px] w-full text-center md:text-left pt-2 px-4 md:py-4"
-                        :class="{ 'bg-red-700/20 dark:bg-yellow-500/20': selectedIndex == index }"
-                        @click="selected(index)">
-                        <h3 class="font-sans leading-none md:leading-normal">{{ experience.employer }}</h3>
+                        class="hidden md:block box-border cursor-pointerfont-sans h-[60px] w-[60vw] md:w-full text-center md:text-left pt-2 px-4 md:py-4 transition-transform duration-500 ease-in-out"
+                        :class="{ 'bg-red-700/20 dark:bg-yellow-500/20': selectedIndex == index }" @click="selected(index)">
+                        <h3 class="font-sans leading-none md:leading-normal md:w-[25vw] lg:w-[15vw]">{{ experience.employer
+                        }}</h3>
                     </li>
                 </ul>
+                <br />
+
+                <div class="md:hidden flex h-fit w-[82vw] justify-between">
+                    <div class="arrow rotate-180" @click="decrement">
+                        <img class="h-12" :src="RightChevron" :class="{ 'yellow-to-red': !darkMode }" />
+                    </div>
+                    <h1 class="text-center text-lg sm:text-2xl py-2">
+                        {{ experiences[activeIndex].employer }}</h1>
+                    <div class="arrow" @click="increment">
+                        <img class="h-12" :src="RightChevron" :class="{ 'yellow-to-red': !darkMode }" />
+                    </div>
+                </div>
                 <div id="elevator"
                     class="absolute hidden md:block bg-red-700 dark:bg-yellow-500 h-[60px] w-1 transition-transform duration-500 ease-in-out"
                     :style="{ transform: `translateY(${elevatorPosition})` }"></div>
+                <div id="runway" class="absolute block md:hidden bg-white h-1 w-[80vw]"
+                    :style="{ transform: `translateY(60px)` }"></div>
                 <div id="walkalator"
-                    class="absolute block md:hidden bg-red-700 dark:bg-yellow-500 w-[30vw] h-1 transition-transform duration-500 ease-in-out"
+                    class="absolute block md:hidden bg-red-700 dark:bg-yellow-500 w-[20vw] h-1 transition-transform duration-500 ease-in-out"
                     :style="{ transform: `translateY(60px) translateX(${walkalatorPosition})` }"></div>
             </aos-vue>
-            <br class="block md:hidden" />
-            <aos-vue animation="fade-up" :once="true" placement="center-bottom" :duration="750"
-                class="flex flex-col h-fit min-h-[55vh] md:min-h-[50vh] lg:min-h-[40vh] w-fit md:w-[50vw] p-2">
-                <h3 class="font-sans text-xl tracking-wider"><span class="mr-2">{{ selectedExperience.title
-                }}</span> |
-                    <span class="font-medium ml-2 text-red-700 dark:text-yellow-500">{{ selectedExperience.employer
-                    }}</span>
-                </h3>
-                <p class="italic">{{ selectedExperience.date }}</p>
-                <br />
-                <ul class="">
-                    <li v-for="desc in selectedExperience.description" :key="desc" class="list-item mb-2">{{ desc }}
-                    </li>
-                </ul>
-            </aos-vue>
+            <br class="block md:hidden my-10" />
+            <div class="w-[80vw] md:w-full">
+                <aos-vue animation="fade-up" :once="true" placement="center-bottom" :duration="750"
+                    class="flex flex-col h-fit min-h-[55vh] md:min-h-[50vh] lg:min-h-[40vh] w-fit md:w-[50vw] p-2">
+                    <h3 class="font-sans text-xl tracking-wider"><span class="mr-2">{{ selectedExperience.title
+                    }}</span> |
+                        <span class="font-medium ml-2 text-red-700 dark:text-yellow-500">{{ selectedExperience.employer
+                        }}</span>
+                    </h3>
+                    <p class="italic">{{ selectedExperience.date }}</p>
+                    <br />
+                    <ul class="">
+                        <li v-for="desc in selectedExperience.description" :key="desc" class="list-item mb-2">{{ desc }}
+                        </li>
+                    </ul>
+                </aos-vue>
+            </div>
         </div>
     </div>
-
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import ExperienceItems from '../interfaces/ExperienceItems';
+import RightChevron from '../assets/chevron_right.png';
 
 const props = defineProps({
     darkMode: Boolean
@@ -54,11 +70,24 @@ const props = defineProps({
 
 let elevatorPosition = ref('0px');
 let walkalatorPosition = ref('0vw');
+let activeIndex = ref(0);
 
 const elevatorHeight = 60;
-const walkalatorWidth = 30;
+const walkalatorWidth = 20; // 80vw / number of experiences
 
 const experiences: ExperienceItems[] = [
+    {
+        title: "Software Development Engineer",
+        employer: "Amazon",
+        description: [
+            "Scaled up existing interactive video ads delivery service in preparation for Thursday Night Football expected traffic of over 50,000 transactions-per-second (TPS)",
+            "Implemented caching mechanism using GuavaCache to decrease reliance on downstream services and decreased overall latency by at least 30%.",
+            "Prepared and executed load tests using TestNG to ensure service reliability during expected peak traffic",
+            "R&D on possible ways to introduce interactive ads onto other video platforms",
+            "Successfully mitigated high-severity production issues while on-call and proactively monitored systems using Cloudwatch dashboards and logs ",
+        ],
+        date: '2022 - Current',
+    },
     {
         title: "Developer",
         employer: "New York Stock Exchange",
@@ -98,10 +127,25 @@ let selectedExperience = ref<ExperienceItems>(experiences[0]);
 let selectedIndex = ref<number>(0);
 
 function selected(index: number) {
+    activeIndex.value = index;
     elevatorPosition.value = `${index * elevatorHeight}px`;
     walkalatorPosition.value = `${index * walkalatorWidth}vw`;
     selectedExperience.value = experiences[index];
     selectedIndex.value = index;
+}
+
+function increment() {
+    if (activeIndex.value < experiences.length - 1) {
+        activeIndex.value++;
+        selected(activeIndex.value);
+    }
+}
+
+function decrement() {
+    if (activeIndex.value > 0) {
+        activeIndex.value--;
+        selected(activeIndex.value);
+    }
 }
 </script>
 
