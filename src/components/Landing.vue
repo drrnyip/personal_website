8b7869115d8dbbdda79b0ca03c810956dbb8debd
Darren Yip <drrnyip@gmail.com>
2024-03-12 21:29:28 -0400

    add brushstroke delay

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="landing"
         class="flex flex-col justify-start h-full min-h-[95vh] w-full md:text-left bg-gray-100 dark:bg-gray-800">
-        <div class="absolute top-[25%] md:top-[30%] lg:top-[35%] px-4 sm:px-12 md:px-28 lg:px-48">
-            <Brushstroke :darkMode="darkMode" :delay="500">
+        <div class="absolute top-[25%] md:top-[30%] lg:top-[35%] px-4 sm:px-12 md:px-28 lg:px-36 xl:px-48">
+            <Brushstroke :darkMode="darkMode" :delay="getBrushstrokeDelay()">
                 <aos-vue animation="fade-down" :once="true" placement="top-bottom" :duration="750" easing="ease"
                     :delay="250">
                     <h3
@@ -13,16 +13,16 @@
                 <aos-vue animation="fade-up" :once="true" placement="top-bottom" :duration="750" easing="ease"
                     :delay="500">
                     <h3
-                        class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-serif text-gray-800 dark:text-gray-50 tracking-widest font-semibold">
-                        I <span id="typewriter"></span></h3>
+                        class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-serif text-gray-800 dark:text-gray-50 tracking-widest font-semibold min-h-fit">
+                        I<span id="typewriter"></span></h3>
                 </aos-vue>
                 <br />
                 <aos-vue animation="fade-up" :once="true" placement="top-bottom" :duration="750" easing="ease"
                     :delay="750">
                     <p
                         class="text-md sm:text-lg md:text-xl lg:text-2xl font-sans text-gray-800 dark:text-gray-50 tracking-wide">
-                        I'm a software engineer with experience in building scalable, secure web applications on
-                        modern cloud architecture.
+                        I'm a software engineer with extensive experience in building scalable software solutions on
+                        modern cloud architecture. 
                     </p>
                 </aos-vue>
             </Brushstroke>
@@ -46,14 +46,17 @@ watch(() => props.darkMode, (curr, prev) => {
 })
 
 const headlines = [
-    "create websites",
-    "love Typescript",
-    "built this with Vue.js",
-    "develop software",
-    "engineer systems",
-    "test my code",
-    "document my code",
-    "work well in a team",
+    " create websites",
+    " am a Full Stack <br>Developer",
+    " love <br>Typescript",
+    " built this with Vue.js",
+    " develop software",
+    " design scalable systems",
+    " always test my code",
+    " document my code",
+    " work well in teams",
+    "'ve worked at startups",
+    "'ve worked at Big Tech",
 ]
 
 let typewriter: any;
@@ -88,6 +91,15 @@ function changeHeadline(index?: number) {
     typewriter.typeString(headline);
     typewriter.pauseFor(500);
     typewriter.start();
+}
+
+function getBrushstrokeDelay(): number {
+    let delay = 700;
+    const length = headlines[currentHeadlineIndex].length;
+    if (length >= 20) {
+        delay = 1000;
+    }
+    return delay;
 }
 
 </script>
