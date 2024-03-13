29ac4433aed3d7b412afe43563485b41fc960b71
Darren Yip <drrnyip@gmail.com>
2024-03-13 14:09:25 -0400

    Update copy for Experience

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -13,9 +13,11 @@
                     class="hidden md:flex md:flex-row justify-around w-[80vw] sm:w-[85vw] md:w-full md:flex-col md:border-b-0 md:border-l-4 md:mr-5 md:pr-5">
                     <li v-for="(experience, index) in experiences" :key="`${experience.title}_${experience.employer}`"
                         class="hidden md:block box-border cursor-pointerfont-sans h-[60px] w-[60vw] md:w-full text-center md:text-left pt-2 px-4 md:py-4 transition-transform duration-500 ease-in-out"
-                        :class="{ 'bg-red-700/20 dark:bg-yellow-500/20': selectedIndex == index }" @click="selected(index)">
-                        <h3 class="font-sans leading-none md:leading-normal md:w-[25vw] lg:w-[15vw] whitespace-nowrap">{{
-                            experience.employer }}</h3>
+                        :class="{ 'bg-red-700/20 dark:bg-yellow-500/20': selectedIndex == index }"
+                        @click="selected(index)">
+                        <h3 class="font-sans leading-none md:leading-normal md:w-[25vw] lg:w-[15vw] whitespace-nowrap">
+                            {{
+            experience.employer }}</h3>
                     </li>
                 </ul>
                 <div id="elevator"
@@ -51,9 +53,9 @@
                 <aos-vue animation="fade-up" :once="true" placement="center-bottom" :duration="750"
                     class="flex flex-col h-fit min-h-[55vh] md:min-h-[50vh] lg:min-h-[40vh] w-fit md:w-[50vw] p-2">
                     <h3 class="font-sans text-xl tracking-wider"><span class="mr-2">{{ selectedExperience.title
-                    }}</span> |
+                            }}</span> |
                         <span class="font-medium ml-2 text-red-700 dark:text-yellow-500">{{ selectedExperience.employer
-                        }}</span>
+                            }}</span>
                     </h3>
                     <p class="italic">{{ selectedExperience.date }}</p>
                     <br />
@@ -89,9 +91,12 @@ const experiences: ExperienceItems[] = [
         employer: "Amazon",
         shortenedEmployer: "Amazon",
         description: [
-            "Successfully scaled up existing interactive video ads delivery service in preparation for Thursday Night Football expected traffic of over 50,000 transactions-per-second (TPS)",
-            "Implemented caching mechanism using AWS Elasticache to decrease reliance on downstream services and decreased overall latency by at least 30%.",
-            "Prepared and executed load tests using TestNG to ensure service reliability during expected peak traffic",
+            "Spearheaded the effort to scale up existing interactive video ads delivery service by 100x in preparation for Thursday Night Football's expected traffic of minimum 50,000 transactions-per-second (TPS)",
+            "Designed and wrote Lambda services with high performance and observability. Services could be monitored via Cloudwatch by team members for performance issues / bugs / other metrics.",
+            "Ensured team's microservices communicated efficiently and seamlessly with upstream & downstream services with minimal downtime",
+            "Collaborated with technical program managers (TPM), engineers and other engineering teams on creating, maintaining and running our services efficiently.",
+            "Implemented caching mechanism using Elasticache to decrease reliance on downstream services and decreased overall latency by at least 30%.",
+            "Prepared and executed load tests using TestNG to ensure service reliability during expected peak traffic of 50k - 75K TPS.",
             "R&D on possible ways to introduce interactive ads onto other video platforms",
             "Successfully mitigated high-severity production issues while on-call and proactively monitored systems using Cloudwatch dashboards and logs ",
         ],
@@ -102,6 +107,7 @@ const experiences: ExperienceItems[] = [
         employer: "New York Stock Exchange",
         shortenedEmployer: "NYSE",
         description: [
+            "Played pivotal role in helping Hercules Solutions get acquired by NYSE",
             "Worked with cloud and infrastructure teams to integrate into secured internal Amazon Web Services (AWS) cloud from existing Google Cloud Platform (GCP) servers",
             "Performed major upgrade to Vue 3 by rewriting all components to use Vue 3's Composition API, reducing bundle size by 50% and thereby significantly improving loading speeds and web performance",
             "Authored runbook to facilitate onboarding new team members, cutting down onboarding time in half",
@@ -114,9 +120,8 @@ const experiences: ExperienceItems[] = [
         shortenedEmployer: "Hercules",
         description: [
             "Worked in two-man engineering team to create an enterprise-level secure, scalable fintech platform built on cloud microservice architecture",
-            "Proposed, spearheaded and developed prototype features based on client feedback and integrated into future releases",
+            "Proposed, spearheaded and developed experimental prototype features based on client feedback using Typescript, GraphQL, Node.js, Vue.js, PostgreSQL",
             "Designed and developed robust and reusable Vue.js components, maximizing code re-use and significantly reducing overall size of codebase",
-            "Collaborated with cloud architect to design and implement Node.js/GraphQL/PostgreSQL server-side code",
             "Wrote and implemented unit tests that provided 95% test coverage for entire API layer",
             "Configured, tested and deployed production releases using Kubernetes and Helm and ensured SLA level of 99% uptime/availability",
             "Spearheaded, developed and managed SAML login process for clients and reduced client onboarding time by 60%",
