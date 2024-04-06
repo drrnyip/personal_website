8ed3b526822b6fd4799bc2c01eea745f272c3301
Darren Yip <drrnyip@gmail.com>
2024-04-06 19:28:14 -0400

    Center all elements regardless of screen size

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,15 @@
 <template>
     <div id="about" class="py-12 bg-gray-100 dark:bg-gray-800">
         <aos-vue animation="fade-in" :once="true" placement="center-bottom" :duration="750"
-            class="flex w-full flex-col md:flex-row">
+            class="flex w-full flex-col md:flex-row justify-center">\
             <h2
-                class="text-center md:text-left text-3xl md:text-3xl lg:text-4xl font-mono text-red-700 dark:text-yellow-500 tracking-widest font-semibold my-4 px-4">
+                class="text-center text-3xl md:text-3xl lg:text-4xl font-mono text-red-700 dark:text-yellow-500 tracking-widest font-semibold my-4 px-4">
                 Who dis?</h2>
         </aos-vue>
         <div class="flex flex-col md:flex-row my-10">
-            <aos-vue animation="fade-down" :once="true" placement="center-bottom" :duration="750" class="flex justify-around flex-row md:flex-col">
-                <div
-                    class="md:mr-8 h-48 w-48 ring-2 ring-red-700 dark:ring-yellow-500 rounded-full bg-red-500">
+            <aos-vue animation="fade-down" :once="true" placement="center-bottom" :duration="750"
+                class="flex justify-around flex-row md:flex-col">
+                <div class="md:mr-8 h-48 w-48 ring-2 ring-red-700 dark:ring-yellow-500 rounded-full bg-red-500">
                     <img src="../assets/logo.png" class="" />
                 </div>
             </aos-vue>
@@ -17,15 +17,22 @@
                 <div
                     class="copy m-12 md:m4 text-center md:text-left font-sans text-lg md:text-xl lg:text-2xl text-gray-800 dark:text-gray-50 tracking-wide">
                     <p>
-                        👋 Hi there, my name is Darren Yip, and I'm a software engineer who enjoys building software and problem-solving. I love to learn, value feedback and work well in teams.
+                        👋 Hi there, my name is Darren Yip, and I'm a software engineer who enjoys building software and
+                        problem-solving. I love to learn, value feedback and work well in teams.
                     </p>
                     <br />
                     <p>
-                        Prior to <a href="https://www.amazon.com" class="underline" target="_blank">Amazon</a>, my industry experience came from the <a
-                            href="https://www.checkmate.digital" class="underline" target="_blank">startup scene</a>, the last of which got acquired by the <a class="underline" href="https://www.nyse.com/index" target="_blank">New York Stock Exchange</a>. My strongest skillsets lie in my versatility in all layers of the stack, but I particularly enjoy working on the front-end building engaging and memorable UI/UX. 
+                        Prior to <a href="https://www.amazon.com" class="underline" target="_blank">Amazon</a>, my
+                        industry experience came from the <a href="https://www.checkmate.digital" class="underline"
+                            target="_blank">startup scene</a>, the last of which got acquired by the <a
+                            class="underline" href="https://www.nyse.com/index" target="_blank">New York Stock
+                            Exchange</a>. My strongest skillsets lie in my versatility in all layers of the stack, but I
+                        particularly enjoy working on the front-end building engaging and memorable UI/UX.
                     </p>
                     <br />
-                    <p>When not playing RPG games or hunting Koroks in ToTK, I can usually be found reading fantasy novels, creating piano covers / playing in church bands, and catching up on the latest anime episodes.
+                    <p>When not playing RPG games or hunting Koroks in ToTK, I can usually be found reading fantasy
+                        novels, creating piano covers / playing in church bands, and catching up on the latest anime
+                        episodes.
                     </p>
                 </div>
             </aos-vue>
@@ -41,5 +48,4 @@ const props = defineProps({
 
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
